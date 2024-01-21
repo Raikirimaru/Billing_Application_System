04175a3edd09ecdbcc7df967faf5abda49811036
Raikirimaru <amemawuli8@gmail.com>
2024-01-21 13:05:11 +0000

    feat: models definition

--- invoicing/admin.py
+++ invoicing/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'address', 'sexe', 'age', 'city', 'zip_code')
+    
+class AdminInvoice(admin.ModelAdmin):
+    list_display = ('customer', 'save_by', 'incoice_date_time', 'total', 'last_update_at', 'paid', 'invoice_type')
+    
+admin.site.register (Customer, AdminCustomer)
+admin.site.register (Invoice, AdminInvoice)
+admin.site.register (Article)
--- invoicing/models.py
+++ invoicing/models.py
@@ -1,8 +1,84 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Customer(models.Model):
     """_summary_
 
     Args:
-        models (_type_): _description_
+        models (Customer): Customer model definition
     """
+    SEX_TYPE = (
+        ('M', 'Man'),
+        ('W', 'Woman')
+    )
+    name = models.CharField(max_length=120)
+    email = models.EmailField()
+    phone = models.CharField(max_length=14)
+    address = models.CharField(max_length=60)
+    sexe = models.CharField(max_length=1, choices=SEX_TYPE)
+    age = models.IntegerField()
+    city = models.CharField(max_length=35)
+    zip_code = models.CharField(max_length=16)
+    created_at = models.DateTimeField(auto_now_add=True)
+    save_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    
+    class Meta:
+        verbose_name = "Customer"
+        verbose_name_plural = "Customers"
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Invoice(models.Model):
+    """_summary_
+
+    Args:
+        models (Invoice): Invoice model definition
+    """
+    INVOICE_TYPE = (
+        ('R', 'Reçu'),
+        ('P', 'Proforma facture'),
+        ('F', 'Facture'),
+    )
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    save_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    incoice_date_time = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10000, decimal_places=2)
+    last_update_at = models.DateTimeField(null=True, blank=True)
+    paid = models.BooleanField(default=False)
+    invoice_type = models.CharField(max_length=1, choices=INVOICE_TYPE)
+    comments = models.TextField(null=True, blank=True, max_length=999)
+    
+    class Meta:
+        verbose_name = 'Invoice'
+        verbose_name_plural = 'Invoices'
+    
+    def __str__(self):
+        return f"{self.customer.name}_{self.incoice_date_time}"
+    
+    @property
+    def get_total_amount(self):
+        articles = self.article_set.all()
+        total = sum(article.get_total for article in articles)
+
+class Article(models.Model):
+    """_Summary_
+    
+    Args:
+        models (Article) : Article model definition
+    """  
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    name = models.CharField(max_length=32)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=1000, decimal_places=2)
+    total = models.DecimalField(max_digits=1000, decimal_places=2)
+    
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+    
+    @property
+    def get_total(self):
+        total = self.quantity * self.unit_price
+        return self.name
+    
